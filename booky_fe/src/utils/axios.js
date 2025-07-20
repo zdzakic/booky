94ae6d15278c6f2f62e0423a2ad1d504216bb6f3
Zeljko Dzakic <zdzakic@gmail.com>
2025-07-20 16:19:24 +0200

    dodane .env variable za fe i be

--- booky_fe/src/utils/axios.js
+++ booky_fe/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+//   baseURL: 'http://localhost:8000/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
--- booky_fe/vite.config.js
+++ booky_fe/vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js', // path to the setup file
-    css: true, // if you have css imports in your components
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_DEV_PROXY_TARGET || 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.js', // path to the setup file
+      css: true, // if you have css imports in your components
+    },
+  }
 })
