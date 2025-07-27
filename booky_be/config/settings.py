33021169bcaf9f7c59eb9931f6df8682b48aa160
Zeljko Dzakic <zdzakic@gmail.com>
2025-07-27 14:46:42 +0200

    BE: db in data moved

--- booky_be/config/settings.py
+++ booky_be/config/settings.py
@@ -13,7 +13,8 @@ ENV = os.environ.get("ENV", "production")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Data directory
-DATA_DIR = BASE_DIR / 'data'
+# DATA_DIR = BASE_DIR / 'data'
+DATA_DIR = Path('/app/data') if os.environ.get('RENDER') == 'true' else BASE_DIR / 'data'
 
 # emailing 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
